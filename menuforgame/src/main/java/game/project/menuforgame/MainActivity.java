ea49375a067560d3e57ad621d3f759868eede23f
levebrains <levebrains@yandex.ru>
2020-04-22 20:18:02 +0300

    add Picasso

--- menuforgame/src/main/java/game/project/menuforgame/MainActivity.java
+++ menuforgame/src/main/java/game/project/menuforgame/MainActivity.java
@@ -1,39 +1,68 @@
 package game.project.menuforgame;
 
-import android.content.SharedPreferences;
+import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Gravity;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     int settingsHolder;
+    private Context context;
+    TextView mysettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextView textStartGame = (TextView) findViewById(R.id.start);
-
         textStartGame.setTypeface(Typeface.createFromAsset(
                 getAssets(), "fonts/Radiant.ttf"));
-    }
-
-    private void getSettings() {
-        SharedPreferences settings = getSharedPreferences(GameMenuDemoActivity.PREFS_NAME, 0);
-
-        settingsHolder[0] = settings.getBoolean("sound", true);
-        settingsHolder[1] = settings.getBoolean("music", true);
-        settingsHolder[2] = settings.getBoolean("vibrate", true);
-        settingsHolder[3] = settings.getBoolean("graphics", true);
-
+        context = getApplicationContext();
+        mysettings = (TextView) findViewById(R.id.settings);
+        registerForContextMenu(mysettings);
     }
 
     @Override
-    public void onClick(View v) {
-   getIntent();
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        menu.setHeaderTitle("Контекстное меню");
+    }
 
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.sound:
+                mysettings.setTextColor(Color.GREEN);
+                System.out.println("приевет");
+                Toast.makeText(context, "Настроить звук", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.music:
+                System.out.println("приевет");
+                Toast toast = Toast.makeText(context, "Второй пункт", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                return true;
+            case R.id.vibrate:
+                System.out.println("Настройка вибрации");
+                return true;
+            case R.id.graphics:
+                System.out.println("Настройка графики");
+                return true;
+        }
+        return super.onContextItemSelected(item);
     }
 }
--- newapp/src/main/java/game/project/newapp/MainActivity.java
+++ newapp/src/main/java/game/project/newapp/MainActivity.java
@@ -1,14 +1,38 @@
 package game.project.newapp;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    VideoView player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        player = (VideoView) findViewById(R.id.videoPlayer);
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video);
+        player.setVideoURI(uri);
+        MediaController controller = new MediaController(this);
+        player.setMediaController(controller);
+        controller.setMediaPlayer(player);
+    }
+
+    public void play(View view) {
+        player.start();
+    }
+
+    public void pause(View view) {
+        player.pause();
+    }
+
+    public void stop(View view) {
+        player.stopPlayback();
+        player.resume();
     }
 }
