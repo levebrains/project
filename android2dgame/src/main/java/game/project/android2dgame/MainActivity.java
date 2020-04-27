0442af9c3bbb9a9ac2aaba121d158e02431d0d3f
levebrains <levebrains@yandex.ru>
2020-04-27 23:07:43 +0300

    add robogame

---
+++ android2dgame/src/main/java/game/project/android2dgame/MainActivity.java
@@ -0,0 +1,24 @@
+package game.project.android2dgame;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set fullscreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Set No Title
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.setContentView(new GameSurface(this));
+    }
+}
